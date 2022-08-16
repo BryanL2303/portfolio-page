8d8495f6bebb322e03c9ca2ab41cefed6631d2c7
BryanL2303 <bryanlee0033@gmail.com>
2022-08-16 09:27:59 +0800

    commit

--- app/javascript/components/project_page/telegram_bot_page/TelegramBotPage.jsx
+++ app/javascript/components/project_page/telegram_bot_page/TelegramBotPage.jsx
@@ -14,7 +14,7 @@ const TelegramBotPage = () => {
     <h1>Telegram Bot: Tracking Finances and Man Hour</h1>
     <div class='text-container'>
     <p>This was a personal project i worked on in February of 2022</p>
-    <p>It started as a whim out of a wish to create an administration bot to help with various tasks</p>
+    <p>It started as a wish to create an administration bot to help with various tasks</p>
     <p>The bot now helps to record expenses as well as how i spend my time each day</p>
     <p>It's functions are run by a Google Apps Script and the data are stored in a Google Docs sheet</p>
     <p>I have also created a separate Google Apps Script to generate the Google Doc Sheets required to work with the Telegram Bot</p>
