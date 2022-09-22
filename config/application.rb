87beac8682a3a3cd3f5a3175fe9dd276a5b51dd0
BryanL2303 <bryanlee0033@gmail.com>
2022-09-22 13:17:39 +0800

    commit

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module PortfolioPage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_record.belongs_to_required_by_default = false
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
+    config.assets.precompile += %w(base., skeleton. .....)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
