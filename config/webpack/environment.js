ff8005c9143a8b3e1afd6a96664e8e7b6fef62d7
BryanL2303 <bryanlee0033@gmail.com>
2022-09-22 13:41:01 +0800

    commit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -17,8 +17,7 @@ environment.config.delete('node.fs')
 environment.config.delete('node.net')
 environment.config.delete('node.tls')
 environment.config.delete('node.child_process')
-environment.config.delete('fallback')
 
-environment.config.merge(customConfig);
+//environment.config.merge(customConfig);
 
 module.exports = environment
