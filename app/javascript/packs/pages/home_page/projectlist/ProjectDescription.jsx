1e0df7e64c3f98d2fa3b6f846dc7d6762a0c7558
BryanL2303 <bryanlee0033@gmail.com>
2022-09-22 18:41:33 +0800

    Added breakline to git and site link

--- app/javascript/packs/pages/home_page/projectlist/ProjectDescription.jsx
+++ app/javascript/packs/pages/home_page/projectlist/ProjectDescription.jsx
@@ -7,6 +7,7 @@ const ProjectDescription = ({project}) => {
       <h1 className='project__name'>{project.attributes.name}</h1>
       <textarea className='project__long-description' readOnly value={project.attributes.longDescription}></textarea>
       {project.attributes.gitLink!=null && <a href={project.attributes.gitLink}>Click here to see the github page!</a>}
+      {project.attributes.gitLink!=null && <br/>}
       {project.attributes.siteLink!=null && <a href={project.attributes.siteLink}>Click here to check out the website!</a>}
       </div>
     )    
