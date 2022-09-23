12a06b0a67f1b0bcb59735fb99d64eca9d968cc6
BryanL2303 <bryanlee0033@gmail.com>
2022-09-23 13:29:24 +0800

    Increased treshold again

--- app/javascript/packs/pages/home_page/ProjectList.jsx
+++ app/javascript/packs/pages/home_page/ProjectList.jsx
@@ -24,7 +24,7 @@ const ProjectList = () => {
     let projectDisplays = document.getElementsByClassName('project__display')
     if (projectDisplays.length > 0) {
       let projectList = document.getElementsByClassName('projects__list')[0]
-      if (screen.width > 700) {
+      if (screen.width > 750) {
         projectList.style['height'] = `${320 + ((projectDisplays.length
          - 1 - ((projectDisplays.length - 1) %3))/3)*320}px`
         let x = 0
